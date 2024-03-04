ebde4644dda43012e0ccaab630a78a87b8174b26
vishal <rajput.vishal.rajes@gmail.com>
2024-03-04 21:28:59 +0530

    xyz_project

--- variable.tf
+++ variable.tf
@@ -1,21 +1,17 @@
 
 variable "ami" {
     type = string
-    default = var.ami
 }
 
 
 variable "instance_type" {
     type = string
-    default = var.instance_type
 }
 
 variable "key_name" {
     type = string
-    default = var.key
 }
 
 variable "vpc_security_group_id" {
     type = list(string)
-    default = [ "sg-03eb9c454196b950d" ]
 }
