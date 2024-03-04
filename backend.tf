efe4be387dd4470ab92a91c6c4f4bc3daae9c312
vishal <rajput.vishal.rajes@gmail.com>
2024-03-04 21:23:52 +0530

    xyz_project

--- backend.tf
+++ backend.tf
@@ -7,13 +7,13 @@ resource "aws_s3_bucket" "backend" {
   }
 
 }
-resource "aws_dynamodb_table" "statelock" {
-    name = "state-lock"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
+# resource "aws_dynamodb_table" "statelock" {
+#     name = "state-lock"
+#     billing_mode = "PAY_PER_REQUEST"
+#     hash_key = "LockID"
 
-    attribute {
-      name = "LockID"
-      type = "S"
-    }
-}
+#     attribute {
+#       name = "LockID"
+#       type = "S"
+#     }
+# }
