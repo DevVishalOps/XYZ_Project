8defeadfb84fb8cd84c316fdc0c587d15c407f64
vishal <rajput.vishal.rajes@gmail.com>
2024-03-05 11:28:36 +0530

    Update_XYZ_project

--- ansible.sh
+++ ansible.sh
@@ -17,3 +17,37 @@ sudo apt install -y ansible
 
 # Display installed versions
 ansible --version
+
+
+#####################################################################
+## PORT NO OF Prometheus :- 9090
+
+sudo apt update -y
+# sudo su -
+# export RELEASE="2.2.1"
+sudo useradd --no-create-home --shell /bin/false prometheus  
+sudo useradd --no-create-home --shell /bin/false node_exporter
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /var/lib/prometheus
+cd /opt/
+wget https://github.com/prometheus/prometheus/releases/download/v2.26.0/prometheus-2.26.0.linux-amd64.tar.gz
+sha256sum prometheus-2.26.0.linux-amd64.tar.gz
+tar -xvf prometheus-2.26.0.linux-amd64.tar.gz
+cd prometheus-2.26.0.linux-amd64
+ls
+sudo cp /opt/prometheus-2.26.0.linux-amd64/prometheus /usr/local/bin/
+sudo cp /opt/prometheus-2.26.0.linux-amd64/promtool /usr/local/bin/
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo cp -r /opt/prometheus-2.26.0.linux-amd64/consoles /etc/prometheus
+sudo cp -r /opt/prometheus-2.26.0.linux-amd64/console_libraries /etc/prometheus
+sudo cp -r /opt/prometheus-2.26.0.linux-amd64/prometheus.yml /etc/prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /etc/prometheus/prometheus.yml
+prometheus --version
+promtool --version
+sudo ufw allow 9090/tcp
+cd /etc/prometheus
