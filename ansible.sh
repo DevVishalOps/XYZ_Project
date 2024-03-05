cd0f0a956ca8355047aecaa6780cebb0e14d31c3
vishal <rajput.vishal.rajes@gmail.com>
2024-03-05 11:01:07 +0530

    Update_XYZ_project

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-sudo apt-get update
+
+# Update the package list
+sudo apt update
+
+# Install software-properties-common to add the Ansible PPA
+sudo apt install -y software-properties-common
+
+# Add the Ansible PPA
+sudo add-apt-repository -y ppa:ansible/ansible
+
+# Update the package list again
+sudo apt update
+
+# Install Ansible
+sudo apt install -y ansible
+
+# Display installed versions
+ansible --version
